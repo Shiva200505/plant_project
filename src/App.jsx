5f18aa1f804f498193be9cdcc559493bb3096f6b
Shiva200505 <shivam.sawant23@vit.edu>
2026-04-09 23:55:19 +0530

    initial commitss

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import Cart from './pages/Cart'
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/products" element={<Products />} />
-      <Route path="/cart" element={<Cart />} />
-    </Routes>
+  <Route path="/plant_project/" element={<Landing />} />
+  <Route path="/plant_project/products" element={<Products />} />
+  <Route path="/plant_project/cart" element={<Cart />} />
+</Routes>
   )
 }
